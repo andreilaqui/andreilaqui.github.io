d368878bcccae128378005c9fdc660ba798714b9
andreilaqui <dreilaqui@yahoo.com>
2024-12-18 18:39:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
+/* place click listener for all toggle buttons  */
 document.querySelectorAll('.toggle-button').forEach(button => {
     button.addEventListener('click', function() {
         const content = this.parentElement.querySelector('.collapse-div');
+        /* either expand or collapse example div */
         if (content.style.display === 'none' || content.style.display === '') {
             content.style.display = 'block';
             this.textContent = '−';
